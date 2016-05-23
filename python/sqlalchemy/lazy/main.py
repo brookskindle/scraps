b313be18e93a32f36209066afe9e5b913193493f
Brooks Kindle <brooks.kindle@wsu.edu>
2016-05-22 23:36:38 -0700

    WIP: Add sqlalchemy example.

---
+++ python/sqlalchemy/lazy/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""
+To be ran in python 3.
+
+This is a sample script that is meant to test the performance of the different
+SQLAlchemy relationship loading techniques. If you're not sure what this is,
+take a look at
+http://docs.sqlalchemy.org/en/rel_1_0/orm/loading_relationships.html
+"""
