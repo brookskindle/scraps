7b8f195dcfc1847d8841a83caf685c3492b3b384
Brooks Kindle <brookskindle+git@gmail.com>
2019-04-10 15:15:40 -0700

    Add react-counter example

---
+++ react/react-counter/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Counter from './Counter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+  return {
+    count: 42
+  };
+}
+
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <Counter/>
+  </Provider>
+);
+
+render(<App />, document.getElementById('root'));
